6a8a9de034e3119face56be8fe02ed30f06f332f
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2019-11-05 18:09:33 +0100

    Bump internal version number to 1.2.0

--- io_import_pokemon_masters.py
+++ io_import_pokemon_masters.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Import Pokémon Masters Models",
     "author": "Turk",
-    "version": (1, 1, 0),
+    "version": (1, 2, 0),
     "blender": (2, 80, 0),
     "location": "File > Import-Export",
     "description": "A tool designed to import LMD files from the mobile game Pokémon Masters",
